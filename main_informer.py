1240c06d14198f510b86673304fb54cf28ab8171
xinyyhu <xinyy.hu@gmail.com>
2024-01-02 20:10:14 +0800

    first

--- main_informer.py
+++ main_informer.py
@@ -19,7 +19,7 @@ parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='l
 parser.add_argument('--seq_len', type=int, default=96, help='input sequence length of Informer encoder')
 parser.add_argument('--label_len', type=int, default=48, help='start token length of Informer decoder')
 parser.add_argument('--pred_len', type=int, default=24, help='prediction sequence length')
-# Informer decoder input: concat[start token series(label_len), zero padding series(pred_len)]
+# Informer decoder input: concat[start token series(label_len), zero padding series(pred_len)]  #
 
 parser.add_argument('--enc_in', type=int, default=7, help='encoder input size')
 parser.add_argument('--dec_in', type=int, default=7, help='decoder input size')
